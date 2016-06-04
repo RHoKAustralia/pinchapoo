e84def10efb2380b97e579305903597c76bed3fd
Samara N <sneilson@swin.edu.au>
2016-06-04 19:49:36 +1000

    Testing child pages

--- wp-content/themes/pinchapoo/index.php
+++ wp-content/themes/pinchapoo/index.php
@@ -52,6 +52,27 @@ Author URI: http://www.rhokaustralia.org/
                                 <!-- < ?php wp_link_pages(); ?> </div>
                             <!--end entry-->
                             <div class="post-footer">
+                               <?php function wpb_list_child_pages() {
+
+                                global $post;
+
+                                if ( is_page() && $post->post_parent )
+
+                                $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
+                                else
+                                $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+
+                                if ( $childpages ) {
+
+                                $string = '<ul>' . $childpages . '</ul>';
+                                }
+
+                                return $string;
+
+                                }
+
+                                add_shortcode('wpb_childpages', 'wpb_list_child_pages');
+                               ?>
                                 <!-- <div class="comments">< ?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
                             </div><!--end post footer-->
                         </div><!--end post-->
