00e77672af7a2c6e2993d1ca11bcd80173fa7caf
Samara N <sneilson@swin.edu.au>
2016-06-04 18:36:51 +1000

    Post!

--- wp-content/themes/pinchapoo/index.php
+++ wp-content/themes/pinchapoo/index.php
@@ -28,47 +28,52 @@ Author URI: http://www.rhokaustralia.org/
 
         <!-- Content -->
 
-        <?php if ( have_posts() ) : ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <div class="post-header">
-                        <div class="date"><?php the_time( 'M j y' ); ?></div>
-                        <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                        <div class="author"><?php the_author(); ?></div>
-                    </div><!--end post header-->
-                    <div class="entry clear">
-                        <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
-                        <?php the_content(); ?>
-                        <?php edit_post_link(); ?>
-                        <?php wp_link_pages(); ?> </div>
-                    <!--end entry-->
-                    <div class="post-footer">
-                        <div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
-                    </div><!--end post footer-->
-                </div><!--end post-->
-            <?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
-            <div class="navigation index">
-                <div class="alignleft"><?php next_posts_link( 'Older Entries' ); ?></div>
-                <div class="alignright"><?php previous_posts_link( 'Newer Entries' ); ?></div>
-            </div><!--end navigation-->
-        <?php else : ?>
-        <?php endif; ?>
-
-
         <section class="main">
             <div class="inner">
 
-                <header class="major">
-                    <span class="category">In Depth</span>
-                    <h2><a href="#">Etiam feugiat tempus veroeros lorem</a></h2>
+
+
+                <?php if ( have_posts() ) : ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <!-- <div class="post-header"> -->
+                                <!--<div class="date">< ?php the_time( 'M j y' ); ?></div> -->
+                            <header class="major">
+                                <span class="category">a cheeky way to help</span>
+                                <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                                <ul class="meta"></ul>
+                            </header>
+                                <!--<div class="author">< ?php the_author(); ?></div> -->
+                            <!--</div><!--end post header-->
+                            <div class="entry clear">
+                                <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
+                                <?php the_content(); ?>
+                                <!--< ?php edit_post_link(); ?> -->
+                                <!-- < ?php wp_link_pages(); ?> </div>
+                            <!--end entry-->
+                            <div class="post-footer">
+                                <!-- <div class="comments">< ?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
+                            </div><!--end post footer-->
+                        </div><!--end post-->
+                    <?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
+                    <!-- <div class="navigation index">
+                        <div class="alignleft">< ?php next_posts_link( 'Older Entries' ); ?></div>
+                        <div class="alignright">< ?php previous_posts_link( 'Newer Entries' ); ?></div>
+                    </div><!--end navigation-->
+                <?php else : ?>
+                <?php endif; ?>
+
+
+
+                   <!--  <h2><a href="#">Etiam feugiat tempus veroeros lorem</a></h2>
                     <ul class="meta">
                         <li>3 days ago</li>
                         <li><a href="#" class="favorites">2,174</a></li>
                         <li><a href="#" class="comments">1,423</a></li>
-                    </ul>
-                </header>
+                    </ul> -->
 
-                <a href="#" class="image main"><img src="<?php bloginfo('template_url'); ?>/images/pic04.jpg" alt="" /></a>
+
+               <!-- <a href="#" class="image main"><img src="<?php bloginfo('template_url'); ?>/images/pic04.jpg" alt="" /></a>
 
                 <p>In ut odio eu quam consectetur tristique nec non nisl. Maecenas porttitor vestibulum augue, nec sodales eros blandit non. Phasellus libero nibh, erat blandit, aliquet volutpat purus. Nullam pretium sed turpis lorem, ac congue orci. Donec pulvinar sagittis pellentesque. In ut odio eu quam consectetur tristique nec non nisl. Maecenas porttitor vestibulum augue, nec sodales eros blandit non. Vis accumsan feugiat adipiscing nisl amet adipiscing accumsan blandit accumsan sapien blandit ac amet faucibus aliquet placerat commodo. Interdum ante aliquet commodo accumsan vis phasellus adipiscing. Ornare a in lacinia. Vestibulum accumsan ac metus massa tempor. Accumsan in lacinia ornare massa amet. Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan.</p>
 
@@ -78,7 +83,7 @@ Author URI: http://www.rhokaustralia.org/
                 <h3>Erat condimentum</h3>
                 <p>Interdum ante aliquet commodo accumsan vis phasellus adipiscing. Ornare a in lacinia. Vestibulum accumsan ac metus massa tempor. Accumsan in lacinia ornare massa amet. Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan. Amet nibh adipiscing adipiscing. Commodo ante vis placerat interdum massa massa primis. Tempus condimentum tempus non ac varius cubilia adipiscing placerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing.</p>
                 <p>Amet nibh adipiscing adipiscing. Commodo ante vis placerat interdum massa massa primis. Tempus condimentum tempus non ac varius cubilia adipiscing placerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet blandit adipiscing arcu ante.</p>
-
+-->
             </div>
         </section>
 
