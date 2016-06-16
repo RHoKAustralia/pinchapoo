754c59f8a2fc1e886a606c973d4cdc00cb56b397
Samara N <sneilson@swin.edu.au>
2016-06-16 20:48:04 +1000

    Footer

--- wp-content/themes/pinchapoo/footer.php
+++ wp-content/themes/pinchapoo/footer.php
@@ -3,7 +3,7 @@
         <section>
             <!--<h2>Pinchapoo</h2>
             <p>Ipsum dolor sit amet tempus adipiscing amet sed aliquam magna etiam magna feugiat cursus lorem nisi. Amet mauris vitae mi dapibus nulla dictum. Ipsum dolor sit amet tempus etiam adipiscing amet sed aliquam veroeros etiam feugiat veroeros aliquam feugiat cursus lorem nisi. Ut mauris vitae mi dapibus nulla dictum. Ipsum dolor sit amet tempus adipiscing tempus.</p> -->
-            <p><img src="<?php bloginfo('template_url'); ?>/images/pinchapoo_big.png" alt="logo" width="80%"/></p>
+            <p><img src="<?php bloginfo('template_url'); ?>/images/pinchapoo_big.png" alt="logo" width="90%"/></p>
             <ul class="icons style2">
                 <li><a href="https://twitter.com/pinchapoo_aus" class="icon fa-twitter" target='_blank'><span class="label">Twitter</span></a></li>
                 <li><a href="https://www.facebook.com/Pinchapoo" target='_blank' class="icon fa-facebook" ><span class="label">Facebook</span></a></li>
@@ -28,7 +28,7 @@
             </form> -->
             <p>While we can’t do what we do without your pinching efforts, financial donations are vital too. We have DGR status, which means that all donations over $2 are tax deductible.
             <br /><br /><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3F3XRE3FSGCUS" target="_blank" style="border-bottom: none">
-                <img alt="Donate with PayPal" src=" https://www.paypalobjects.com/webstatic/en_US/btn/btn_donate_pp_142x27.png" />
+                <img alt="Donate with PayPal" src="/wp-content/themes/pinchapoo/images/paypal_donate_button_200.jpg" width="98%"/>
             </a></p>
             <p>While we encourage the cheeky pinching of your hotel toiletries, we also accept the donation of full size toiletries and other goods.</p>
             <ul class="actions">
