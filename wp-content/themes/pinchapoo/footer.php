c045f1eb08ec21739b7cbe7d3dd3c83531b037f7
Samara N <sneilson@swin.edu.au>
2016-06-16 20:09:31 +1000

    Footer

--- wp-content/themes/pinchapoo/footer.php
+++ wp-content/themes/pinchapoo/footer.php
@@ -27,7 +27,7 @@
                 </ul>
             </form> -->
             <p>While we can’t do what we do without your pinching efforts, financial donations are vital too. We have DGR status, which means that all donations over $2 are tax deductible.
-            <br /><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3F3XRE3FSGCUS" target="_blank" style="border-bottom: none">
+            <br /><br /><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3F3XRE3FSGCUS" target="_blank" style="border-bottom: none">
                 <img alt="Donate with PayPal" src=" https://www.paypalobjects.com/webstatic/en_US/btn/btn_donate_pp_142x27.png" />
             </a></p>
             <p>While we encourage the cheeky pinching of your hotel toiletries, we also accept the donation of full size toiletries and other goods.</p>
