d35a234129a672d7c53d24efe5beeb54d3dd86a8
Ryan Brown <ryan.brown@rea-group.com>
2016-06-16 20:31:17 +1000

    Added blogroll - part 5

--- wp-content/themes/pinchapoo/page-media.php
+++ wp-content/themes/pinchapoo/page-media.php
@@ -60,11 +60,11 @@
 	 				                <article>
 															<?php if ( $bookmark->link_image != null ) {
 																if ( strpos( $bookmark->link_image, 'http' ) === 0 ) {
-																	echo "<img src=\"$bookmark->link_image\" $alt $title />";
+																	echo "<img src=\"$bookmark->link_image\" />";
 																} 
 																else 
 																{ 
-																	echo "<img src=\"" . get_option('siteurl') . "$bookmark->link_image\" $alt $title />";
+																	echo "<img src=\"" . get_option('siteurl') . "$bookmark->link_image\" />";
 																}
 															} 
 													?>
