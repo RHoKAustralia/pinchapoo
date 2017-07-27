899df8482d819315128976964f4481f3909ea698
Samara N <sneilson@swin.edu.au>
2017-07-27 23:45:48 +1000

    Javascript test for team registration

--- wp-content/themes/pinchapoo/index.php
+++ wp-content/themes/pinchapoo/index.php
@@ -254,11 +254,11 @@ Author URI: http://www.rhokaustralia.org/
 
             if(teamSize == 4)
             {
-                location = 'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=pinchapoo@hotmail.com.au&lc=AU&on1=Team%20Name&os1='+teamName+'&item_name=Pinchapoo%20Golf%20Day%20Team%20Registration&amount=1000.00&quantity=1&currency_code=AUD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHostedGuest';
+                location = 'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=pinchapoo@hotmail.com.au&lc=AU&on1=Team%20Name&os1='+teamName+'&item_name=Pinchapoo%20Golf%20Day%20Team%20Registration&amount=1000.00&quantity=1&currency_code=AUD&rm=1&return=http://www.pinchapoo.org.au/get-involved/events/&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHostedGuest';
             }
             else
             {
-                location = 'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=pinchapoo@hotmail.com.au&lc=AU&on1=Team%20Name&os1='+teamName+'&item_name=Pinchapoo%20Golf%20Day%20Individual%20Registration&amount=1.00&quantity='+teamSize+'&currency_code=AUD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHostedGuest';
+                location = 'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=pinchapoo@hotmail.com.au&lc=AU&on1=Team%20Name&os1='+teamName+'&item_name=Pinchapoo%20Golf%20Day%20Individual%20Registration&amount=1.00&quantity='+teamSize+'&currency_code=AUD&rm=1&return=http://www.pinchapoo.org.au/get-involved/events/&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHostedGuest';
             }
         }
     }, false );
