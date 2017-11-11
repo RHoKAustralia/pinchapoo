9ab6f35bc98516d70a19615ecdb6ff07d10280a6
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2017-11-11 21:37:29 +1100

    Turned off front-page banner text

--- wp-content/themes/pinchapoo/front-page.php
+++ wp-content/themes/pinchapoo/front-page.php
@@ -31,13 +31,13 @@ Author URI: http://www.rhokaustralia.org/d
         <article class="full">
             <!--<img src="< ?php bloginfo('template_url'); ?>/images/banner_2.png" alt="" data-position="center" /> -->
             <img src="<?php echo get_option('hero_image'); ?>" id="hero_image" alt="" data-position="center" />
-            <div class="content">
+            <!--<div class="content">
                 <h2><a href="#">A CHEEKY WAY TO HELP</a></h2>
                 <p>Pinchapoo has been distributing ‘pinched’ hotel toiletries since 2009, helping improve the quality of life for disadvantaged people around Australia.</p>
                 <ul class="actions">
                     <li><a href="/about-us/what-is-pinchapoo/" class="button">Read More</a></li>
                 </ul>
-            </div>
+            </div> -->
         </article>
 				
 				<article class="half pinchapoo-blue squash">
