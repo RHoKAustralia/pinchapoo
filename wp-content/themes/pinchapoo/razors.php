37ef0a88c573413b28431908460c2227e30cc9e1
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2017-11-12 18:30:00 +1100

    Added razor counter auto-update

--- wp-content/themes/pinchapoo/razors.php
+++ wp-content/themes/pinchapoo/razors.php
@@ -1,15 +1,28 @@
+<html>
+<head></head>
+<body>
+<form id='goForm' method="post" action="http://www.pinchapoo.org.au/"></form>
+<?php
+//echo "updating....";
+$razorCount = (int) get_option('number_of_razors');
+$newCount = (int) $_GET['count'];
 
-    <?php
-   //echo "updating....";
-    $razorCount = get_option('number_of_razors');
-    $newCount = (int) $_GET['count'];
+$new_value = $razorCount + $newCount;
 
-    $new_value = $razorCount + $newCount;
+update_option( 'number_of_razors' , $new_value );
 
-    update_option( 'number_of_razors' , $new_value );
+?><script>
 
-    echo '<script>
-           window.location = "http://www.pinchapoo.org.au";
-          </script>';
-    ?>
+    setTimeout(sendForm, 1000);
+
+    function sendForm()
+    {
+        document.getElementById("goForm").submit();
+    }
+
+</script>
+</body>
+</html>
+
+
 
